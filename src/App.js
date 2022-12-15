01fb7cd4e9a9d73910e28f1bdd0343ec27ba3309
Pawel Jan Wilk <pwilk0108@gmail.com>
2022-12-15 16:33:22 +0100

    changed the router component to be more elegant

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import {router, RouterProvider} from './routes/index'
+import Router from './routes/index'
 
 function App() {
   return (
     <div className="App">
-      <RouterProvider router={router}/>
+      <Router />
     </div>
   );
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,24 +1,26 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import LibraryRoute from "./Library";
 import HomeRoute from './Home'
 import AboutRoute from './About'
 import ErrorRoute from './Error'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomeRoute />,
-    errorElement: <ErrorRoute />,
-  },
-  {
-    path: "/library",
-    element: <LibraryRoute />
-  },
-  {
-    path: "/about",
-    element: <AboutRoute />
-  }
-]);
+function Router(){
+  return (
+    <BrowserRouter>
+      <div id="navbar__container">
+        <Link to="/"> Home </Link>
+        <Link to="/library"> Library </Link>
+        <Link to="/about"> About </Link>
+      </div>
 
-export {router, RouterProvider};
+      <Routes>
+        <Route path="/" element={ <HomeRoute /> } errorElement={ <ErrorRoute /> } />
+        <Route path="/library" element={ <LibraryRoute /> } />
+        <Route path="/about" element={ <AboutRoute /> } />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Router;
