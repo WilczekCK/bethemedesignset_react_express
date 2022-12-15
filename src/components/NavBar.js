f63b8f2f62a66da4224447cbeaf93bc01bd224f5
Pawel Jan Wilk <pwilk0108@gmail.com>
2022-12-15 16:52:17 +0100

    used the mui grid to build basic nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,35 @@
 import React from "react";
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Grid2 from '@mui/material/Unstable_Grid2'; // Grid version 2
 import { Link } from "react-router-dom";
+
+const Item = styled(Paper)(({ }) => ({
+  backgroundColor: 'grey',
+  textAlign: 'center',
+  color: 'white',
+  textDecoration: 'none'
+}));
 
 function NavBar(){
   return (
-      <div id="navbar__container">
-        <Link to="/"> Home </Link>
-        <Link to="/library"> Library </Link>
-        <Link to="/about"> About </Link>
-      </div>
+        <Grid2 container spacing={3}>
+            <Grid2 md={4}>
+                <Item>
+                    <Link to="/"> Home </Link>
+                </Item>
+            </Grid2>
+            <Grid2 md={4}>
+                <Item>
+                    <Link to="/library"> Library </Link>
+                </Item>
+            </Grid2>
+            <Grid2 md={4}>
+                <Item>
+                    <Link to="/about"> About </Link>
+                </Item>
+            </Grid2>
+        </Grid2>
   )
 }
 
