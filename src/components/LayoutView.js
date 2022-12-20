dc0a10eb67d2cd817bf220f174442fa814808ae5
Pawel Jan Wilk <pwilk0108@gmail.com>
2022-12-20 15:32:05 +0100

    first config, a basis of iframeView

--- src/components/LayoutView.js
+++ src/components/LayoutView.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import IframeResizer from 'iframe-resizer-react';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+
+
+const LayoutView__container = styled(Paper)(({ }) => ({
+  width: '1240px',
+  margin: '0 auto',
+}));
 
 function LayoutView() {
   return (
-    <div className="LayoutView__container">
+    <LayoutView__container>
       <IframeResizer
             log
             src="https://themes.muffingroup.com/be/marketing2/"
-            style={{ width: '1px', minWidth: '100%'}}
+            style={{ width: '1240px' }}
+            scrolling={true}
         />
-    </div>
+    </LayoutView__container>
   );
 }
 
