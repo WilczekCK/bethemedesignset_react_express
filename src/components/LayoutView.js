db3ff3227e908c1b4e3928c0296d4ce8d1311e95
Pawel Jan Wilk <pwilk0108@gmail.com>
2022-12-20 15:34:52 +0100

    added button group above the iframe

--- src/components/LayoutView.js
+++ src/components/LayoutView.js
@@ -2,6 +2,8 @@ import React from 'react';
 import IframeResizer from 'iframe-resizer-react';
 import Paper from '@mui/material/Paper';
 import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
 
 
 const LayoutView__container = styled(Paper)(({ }) => ({
@@ -9,9 +11,20 @@ const LayoutView__container = styled(Paper)(({ }) => ({
   margin: '0 auto',
 }));
 
+function BasicButtonGroup() {
+  return (
+    <ButtonGroup variant="outlined" aria-label="outlined button group">
+      <Button>Mobile</Button>
+      <Button>Tablet</Button>
+      <Button>Desktop</Button>
+    </ButtonGroup>
+  )
+}
+
 function LayoutView() {
   return (
     <LayoutView__container>
+      <BasicButtonGroup />
       <IframeResizer
             log
             src="https://themes.muffingroup.com/be/marketing2/"
