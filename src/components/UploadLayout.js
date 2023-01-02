77c8b1aff0bbf82a4a7e1b8faacf3a5538e46c92
Pawel Jan Wilk <pwilk0108@gmail.com>
2023-01-02 13:57:22 +0100

    grid from mui attached

--- src/components/UploadLayout.js
+++ src/components/UploadLayout.js
@@ -1,10 +1,15 @@
+import React, { useState } from 'react';
+import Grid2 from '@mui/material/Unstable_Grid2'; // Grid version 2
 
 export default function Render(){
     return (
-        <>
-            <form>
-                <input type="text"></input>
-            </form>
-        </>
+        <Grid2 container justifyContent={'center'}>
+            <Grid2 md={4} xs={8} style={{'border': '2px solid black'}}>
+                <h3> Paste the code you got from export tool </h3>
+                <form>
+                    <input type="text"></input>
+                </form>
+            </Grid2>
+        </Grid2>
     );
 }
