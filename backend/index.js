1c8b4a3af45b02bfddca71ffa27fe1e584e9ccef
Pawel Jan Wilk <pwilk0108@gmail.com>
2023-01-03 12:45:44 +0100

    simple auth, for now its just a querysting check

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,25 @@
+require("dotenv").config();
 const express = require("express");
 
 const app = express();
 const port = 3000;
+const secret = process.env.SECRET_KEY;
+
+const isAuthorizedToRoute = (req, res, next) => {
+    if (req.query.secret_key !== secret) {
+        res.sendStatus(401);
+    } else {
+        next();
+    }
+}
 
 app.get("/", (req, res) => {
   res.send(`Hello world!`);
 });
+
+app.get('/protected', isAuthorizedToRoute, (req, res) => {
+    res.send('Authorized');
+})
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
