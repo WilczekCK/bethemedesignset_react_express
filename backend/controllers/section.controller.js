bc4420d41af3471f426f6d948f93ea72a717d518
Pawel Jan Wilk <pwilk0108@gmail.com>
2023-01-04 12:32:10 +0100

    section -> sections route name

--- backend/controllers/section.controller.js
+++ backend/controllers/section.controller.js
@@ -4,14 +4,17 @@ const Section = require('../models/section');
 class SectionController {
     whereObj = {}; // for getter
 
+    setWhereObj( where ) {
+        this.whereObj = where;
+        return this;
+    }
+
     get allRecords() {
         return (async () => await Section.findAll())();
     }
 
     get records() {
-        return (async () => {
-            
-        })()
+        return (async () => await Section.findOne({ where }))();
     }
 
     constructor(){
--- backend/routes/sections.js
+++ backend/routes/sections.js
@@ -2,10 +2,16 @@
 module.exports = function(app, isAuthorizedToRoute){
     const sectionController = require('../controllers/section.controller');
 
-    app.route('/section')
+    app.route('/sections')
         .get(async (req, res) => {
-            const getAllRecords = await sectionController.allRecords;
-            res.send( getAllRecords );
+            if (where = req.query.where) {
+                const parsedJSON = JSON.parse(where);
+
+                console.log(parsedJSON);
+            } else {
+                const getAllRecords = await sectionController.allRecords;
+                res.send( getAllRecords );
+            }
         })
         .post(isAuthorizedToRoute, (req,res) => {
             res.send('POST route');
