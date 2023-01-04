24646a4136abf4c18f45e110e53ca41ead413ef4
Pawel Jan Wilk <pwilk0108@gmail.com>
2023-01-04 12:11:48 +0100

    the init of section controller file

---
+++ backend/controllers/section.controller.js
@@ -0,0 +1,10 @@
+const Editor = require('../models/editor');
+const Section = require('../models/section');
+
+class SectionController {
+    constructor(){
+
+    }
+}
+
+module.exports = new SectionController();
