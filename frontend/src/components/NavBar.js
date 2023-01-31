b4ef435f224c0a33b7fe9ec8fe75b695ad202f56
Pawel Jan Wilk <pwilk0108@gmail.com>
2023-01-31 15:40:51 +0100

    header grid created, fully responsive (no burger)

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -5,29 +5,47 @@ import Grid2 from '@mui/material/Unstable_Grid2'; // Grid version 2
 import { Link } from "react-router-dom";
 
 const Item = styled(Paper)(({ }) => ({
-  backgroundColor: 'grey',
   textAlign: 'center',
   color: 'white',
-  textDecoration: 'none'
+  textDecoration: 'none',
+  backgroundColor: 'transparent'
 }));
 
 function NavBar(){
   return (
-        <Grid2 container spacing={3}>
-            <Grid2 md={4}>
-                <Item>
-                    <Link to="/"> Home </Link>
-                </Item>
+        <Grid2 container backgroundColor="var(--backgroundDarkerGrey)" minHeight={100} md={12} display="flex" alignContent="center">
+            <Grid2 container md={1} mdOffset={1} xsOffset={.5}>
+                <Link to="/"> LOGO </Link>
             </Grid2>
-            <Grid2 md={4}>
-                <Item>
-                    <Link to="/library"> Library </Link>
-                </Item>
-            </Grid2>
-            <Grid2 md={4}>
-                <Item>
-                    <Link to="/about"> About </Link>
-                </Item>
+            
+            <Grid2 container md={8} xs={8} padding="0px 50px">
+                    <Grid2 xs={3} md={3}>
+                        <Link to="/library"> Library </Link>
+                    </Grid2>
+                
+                    <Grid2 xs={3} md={3}>
+                        <Link to="/about"> About </Link>
+                    </Grid2>
+
+                    <Grid2 xs={3} md={3}>
+                        <Link to="/halloffame"> Hall of Fame </Link>
+                    </Grid2>
+
+                    <Grid2 xs={3} md={3}>
+                        <Link to="/instructions"> Instructions </Link>
+                    </Grid2>
+            </Grid2> 
+
+            <Grid2 container md={1} mdOffset={0} xsOffset={.5}>
+                <Grid2 md={4}>
+                        <Link to="#"> S1 </Link>
+                </Grid2>
+                <Grid2 md={4}>
+                        <Link to="#"> S2 </Link>
+                    </Grid2>
+                <Grid2 md={4}>
+                        <Link to="#"> S3 </Link>
+                </Grid2>
             </Grid2>
         </Grid2>
   )
