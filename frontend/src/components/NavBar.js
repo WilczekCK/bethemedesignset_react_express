706bc3852126e2697208ca73b17aa0994b410ad8
Pawel Jan Wilk <pwilk0108@gmail.com>
2023-02-01 15:15:47 +0100

    divided the desktop and mobile menu in component

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import debounce from 'lodash.debounce';
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import Grid2 from '@mui/material/Unstable_Grid2'; // Grid version 2
 import { Link } from "react-router-dom";
-
 import FacebookIcon from '@mui/icons-material/Facebook';
 
+import useResponsiveDetect from '../hooks/useResponsiveDetect'
 
 const Item = styled(Paper)(({ }) => ({
   textAlign: 'center',
@@ -25,8 +26,73 @@ const Logo = styled(Paper)(({ }) => ({
     textAlign: 'center'
 }));
 
+function setDeviceOnResize(props) {
+    const debounceFn = debounce(() => {
+      let devicesAvailable = useResponsiveDetect(); 
+      props.setDevice(devicesAvailable.actualDeviceName);
+    }, 100);
+      
+    window.addEventListener('resize', () => debounceFn() );
+    window.removeEventListener('resize', () => debounceFn() );
+}
+
+
+function MobileMenu(){
+
+}
+
+function DesktopMenu(){
+    return (
+        <>
+            <Grid2 container md={8} xs={7} justifyContent="center" alignContent="center">
+                        <Grid2 xs={3} md={2}>
+                            <Link to="/library" style={{textDecoration: 'none'}}> 
+                                <Item>Library</Item>
+                            </Link>
+                        </Grid2>
+
+                        <Grid2 xs={3} md={2}>
+                            <Link to="/halloffame" style={{textDecoration: 'none'}}> 
+                                <Item>Hall of Fame</Item>
+                            </Link>
+                        </Grid2>
+
+                        <Grid2 xs={3} md={2}>
+                            <Link to="/instructions" style={{textDecoration: 'none'}}> 
+                                <Item>Instructions</Item>
+                            </Link>
+                        </Grid2>
+
+                        <Grid2 xs={3} md={2}>
+                            <Link to="/about" style={{textDecoration: 'none'}}> 
+                                <Item>About</Item>
+                            </Link>
+                        </Grid2>
+                </Grid2> 
+
+                <Grid2 container md={1} mdOffset={0} xsOffset={.5} justifyContent="center" alignContent="center">
+                    <Grid2 md={4}>
+                        <FacebookIcon fontSize="large" color="primary"/>
+                    </Grid2>
+                    <Grid2 md={4}>
+                        <FacebookIcon fontSize="large" color="primary"/>
+                    </Grid2>
+                    <Grid2 md={4}>
+                        <FacebookIcon fontSize="large" color="primary"/>
+                    </Grid2>
+            </Grid2>
+        </>
+    )
+}
+
+
 function NavBar(){
-  return (
+    let devicesAvailable = useResponsiveDetect();
+    const [device, setDevice] = useState(devicesAvailable.actualDeviceName); // for DeviceSelectedButtons
+    setDeviceOnResize({device, setDevice});
+
+
+    return (
         <Grid2 container backgroundColor="var(--backgroundDarkerGrey)" minHeight={90} md={12} display="flex" alignContent="center">
             <Grid2 container md={1} mdOffset={1} xsOffset={.5}>
                 <Link to="/" style={{fontWeight: 700, textDecoration: 'none'}}> 
@@ -37,45 +103,9 @@ function NavBar(){
                 </Link>
             </Grid2>
             
-            <Grid2 container md={8} xs={7} display="flex" justifyContent="center" alignContent="center">
-                    <Grid2 xs={3} md={2}>
-                        <Link to="/library" style={{textDecoration: 'none'}}> 
-                            <Item>Library</Item>
-                        </Link>
-                    </Grid2>
-
-                    <Grid2 xs={3} md={2}>
-                        <Link to="/halloffame" style={{textDecoration: 'none'}}> 
-                            <Item>Hall of Fame</Item>
-                        </Link>
-                    </Grid2>
-
-                    <Grid2 xs={3} md={2}>
-                        <Link to="/instructions" style={{textDecoration: 'none'}}> 
-                            <Item>Instructions</Item>
-                        </Link>
-                    </Grid2>
-
-                    <Grid2 xs={3} md={2}>
-                        <Link to="/about" style={{textDecoration: 'none'}}> 
-                            <Item>About</Item>
-                        </Link>
-                    </Grid2>
-            </Grid2> 
-
-            <Grid2 container md={1} mdOffset={0} xsOffset={.5} display="flex" justifyContent="center" alignContent="center">
-                <Grid2 md={4}>
-                    <FacebookIcon fontSize="large" color="primary"/>
-                </Grid2>
-                <Grid2 md={4}>
-                    <FacebookIcon fontSize="large" color="primary"/>
-                </Grid2>
-                <Grid2 md={4}>
-                    <FacebookIcon fontSize="large" color="primary"/>
-                </Grid2>
-            </Grid2>
+            {device !== 'mobile' ? <DesktopMenu/> : <MobileMenu/>}
         </Grid2>
-  )
+    )
 }
 
 export default NavBar;
