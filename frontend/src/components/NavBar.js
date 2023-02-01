62843e8df0e2098b430f070689e5f0dd105e20c9
Pawel Jan Wilk <pwilk0108@gmail.com>
2023-02-01 15:54:34 +0100

    the menu has been inserted, beginning

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -5,6 +5,11 @@ import Paper from '@mui/material/Paper';
 import Grid2 from '@mui/material/Unstable_Grid2'; // Grid version 2
 import { Link } from "react-router-dom";
 import FacebookIcon from '@mui/icons-material/Facebook';
+import MenuIcon from '@mui/icons-material/Menu';
+
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem'
+import Button from '@mui/material/Button';
 
 import useResponsiveDetect from '../hooks/useResponsiveDetect'
 
@@ -14,6 +19,16 @@ const Item = styled(Paper)(({ }) => ({
   textDecoration: 'none',
   backgroundColor: 'transparent',
   boxShadow: 'none',
+}));
+
+
+const ItemMobile = styled(Paper)(({ }) => ({
+    textAlign: 'center',
+    color: 'black',
+    textDecoration: 'none',
+    backgroundColor: 'transparent',
+    boxShadow: 'none',
+    padding: '10px 20px'
 }));
 
 const Logo = styled(Paper)(({ }) => ({
@@ -38,39 +53,74 @@ function setDeviceOnResize(props) {
 
 
 function MobileMenu(){
-
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+  
+    return (
+        <Grid2 container xl={1} md={1} xs={1} xlOffset={8} mdOffset={8} smOffset={6.5} xsOffset={5.5}>
+            <Button
+            id="basic-button"
+            aria-controls={open ? 'basic-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+            onClick={handleClick}
+            >
+            <MenuIcon />
+            </Button>
+            <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+                'aria-labelledby': 'basic-button',
+            }}
+            >
+            <Link style={{textDecoration: 'none'}} to="/library"  onClick={handleClose}><ItemMobile>Library</ItemMobile></Link>
+            <Link style={{textDecoration: 'none'}} to="/halloffame" onClick={handleClose}><ItemMobile>Hall of Fame</ItemMobile></Link>
+            <Link style={{textDecoration: 'none'}} to="/instructions"  onClick={handleClose}><ItemMobile>Instructions</ItemMobile></Link>
+            <Link style={{textDecoration: 'none'}} to="/about" onClick={handleClose}><ItemMobile>About</ItemMobile></Link>
+        </Menu>
+    </Grid2>
+    );
 }
 
 function DesktopMenu(){
     return (
         <>
-            <Grid2 container md={8} xs={7} justifyContent="center" alignContent="center">
-                        <Grid2 xs={3} md={2}>
+            <Grid2 container md={6} xs={7} justifyContent="center" alignContent="center">
+                        <Grid2 xs={3} md={3}>
                             <Link to="/library" style={{textDecoration: 'none'}}> 
                                 <Item>Library</Item>
                             </Link>
                         </Grid2>
 
-                        <Grid2 xs={3} md={2}>
+                        <Grid2 xs={3} md={3}>
                             <Link to="/halloffame" style={{textDecoration: 'none'}}> 
                                 <Item>Hall of Fame</Item>
                             </Link>
                         </Grid2>
 
-                        <Grid2 xs={3} md={2}>
+                        <Grid2 xs={3} md={3}>
                             <Link to="/instructions" style={{textDecoration: 'none'}}> 
                                 <Item>Instructions</Item>
                             </Link>
                         </Grid2>
 
-                        <Grid2 xs={3} md={2}>
+                        <Grid2 xs={3} md={3}>
                             <Link to="/about" style={{textDecoration: 'none'}}> 
                                 <Item>About</Item>
                             </Link>
                         </Grid2>
                 </Grid2> 
 
-                <Grid2 container md={1} mdOffset={0} xsOffset={.5} justifyContent="center" alignContent="center">
+                <Grid2 container md={1} mdOffset={1} xsOffset={.5} justifyContent="center" alignContent="center">
                     <Grid2 md={4}>
                         <FacebookIcon fontSize="large" color="primary"/>
                     </Grid2>
@@ -94,7 +144,7 @@ function NavBar(){
 
     return (
         <Grid2 container backgroundColor="var(--backgroundDarkerGrey)" minHeight={90} md={12} display="flex" alignContent="center">
-            <Grid2 container md={1} mdOffset={1} xsOffset={.5}>
+            <Grid2 container md={2} mdOffset={1} xsOffset={.5} xs={3.5}>
                 <Link to="/" style={{fontWeight: 700, textDecoration: 'none'}}> 
                     <Logo>Be</Logo>
                     <Item style={{marginTop: '-5px'}}>
@@ -103,7 +153,7 @@ function NavBar(){
                 </Link>
             </Grid2>
             
-            {device !== 'mobile' ? <DesktopMenu/> : <MobileMenu/>}
+            {device === 'desktop' ? <DesktopMenu/> : <MobileMenu/>}
         </Grid2>
     )
 }
